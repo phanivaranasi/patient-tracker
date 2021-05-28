2f9d9f5ba535792a06f8e015f9ab6e38c31de589
Phaneendra Varanasi <varanasikrishnaphaneendra@gmail.com>
2021-05-28 07:13:39 +0530

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../component/layout'
+const Index: React.FunctionComponent = () => {
+  return (
+    <Layout title="Home">
+      <h1>Hello Next.js 👋</h1>
+    </Layout>
+  )
+}
+export default Index
